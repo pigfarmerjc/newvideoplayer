ec459dd59ec0a84f534e1e272411385a30bcae0b
pigfarmerjc <pigfarmerjc@users.noreply.github.com>
2026-08-15 13:51:41 +0800

    build: configure KSP for Room in core-database to resolve Windows sqlite-jdbc compilation issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
 }
